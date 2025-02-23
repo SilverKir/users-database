f0fec5404ccda5fbf23233590e378a3ab6b6ef32
silver_kir <rci@mp03.ru>
2025-02-23 10:13:41 +0800

    role service insert

--- src/main/java/com/example/userData/model/Role.java
+++ src/main/java/com/example/userData/model/Role.java
@@ -4,6 +4,7 @@ import jakarta.persistence.*;
 import lombok.Data;
 
 @Entity
+@Data
 @Table(name = "roles")
 public class Role {
     @Id
---
+++ src/main/java/com/example/userData/services/RoleService.java
@@ -0,0 +1,39 @@
+package com.example.userData.services;
+
+import com.example.userData.model.Role;
+import com.example.userData.model.User;
+import com.example.userData.repository.RoleRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class RoleService {
+    private final RoleRepository roleRepository;
+
+    public List<Role> getAllRoles() {
+        return roleRepository.findAll();
+    }
+
+    public Role addRole(Role role) {
+        return roleRepository.save(role);
+    }
+
+    public Role getRoleById (Long id){
+        return roleRepository.findById(id).orElse(null);
+    }
+
+    public Role updateRole(Long id, Role role) {
+        role.setId(id);
+        return roleRepository.save(role);
+    }
+
+    public Role deleteRole(Long id) {
+        Role deletedRole = getRoleById(id);
+        roleRepository.deleteById(id);
+        return deletedRole;
+    }
+
+}
